7b4b14797b444690178030dca81758064bb9e07f
mohamed shire <m.x.shire876@hotmail.com>
2020-05-02 00:51:19 -0500

    i added myinfo component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import React from "react"
 import ReactDOM from "react-dom"
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"))
+import MyInfo from "./MyInfo"
+ReactDOM.render(<MyInfo />, document.getElementById("root"))
