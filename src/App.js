0d63b9d352c6e72e9ef1ef7709468ba698d51616
mohamed shire <m.x.shire876@hotmail.com>
2020-05-09 04:34:56 -0500

    inline style practice with the todo app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
 import React from "react";
-
-function App() {
-  const date = new Date();
-  const hours = date.getHours();
-  let timeOfDay;
-  const styles ={
-    fontSize: 30
-  }
-  if (hours < 12) {
-    timeOfDay = "morning";
-    styles.color = "grey"
-
-  } else if (hours > 12 && hours < 17) {
-    timeOfDay = "Afternoon";
-    styles.color = "blue"
-
-  } else {
-    timeOfDay = "Night";
-    styles.color = "green"
-  }
-  return <h1 style={styles}>Good{timeOfDay}!</h1>;
+import TodoItem from "./TodoItem"
+function App(){
+  return(
+    <TodoItem />
+  )
 }
 export default App;
---
+++ src/TodoItem.js
@@ -0,0 +1,21 @@
+import React from "react"
+function TodoItem(){
+    return(
+    <div className="text">  
+        <h1>TODO APPLICATION</h1>
+<input type="checkbox" />
+            <p>Placeholder text here</p>
+            
+<input type="checkbox" />
+            <p>Placeholder text here</p>
+            
+<input type="checkbox" />
+            <p>Placeholder text here</p>
+            
+<input type="checkbox" />
+            <p>Placeholder text here</p>
+    </div>
+    )
+}
+
+export default TodoItem
