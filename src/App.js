996702deee8fa6c69b061c4e2b401d7a16647098
mohamed shire <m.x.shire876@hotmail.com>
2020-05-09 04:04:41 -0500

    inline style practice

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from "react";
 
-
 function App() {
- const date = new Date()
- const hours = date.getHours()
- let timeOfDay
- if(hours < 12){
-   timeOfDay = ("morning")
- }
- else if(hours > 12 && hours < 17){
-timeOfDay = ("Afternoon")
- }
- else{
-   timeOfDay = ("Night")
- }
- return(
- <h1>Good{timeOfDay}!</h1>
- )
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
+  const styles ={
+    fontSize: 30
+  }
+  if (hours < 12) {
+    timeOfDay = "morning";
+    styles.color = "grey"
+
+  } else if (hours > 12 && hours < 17) {
+    timeOfDay = "Afternoon";
+    styles.color = "blue"
+
+  } else {
+    timeOfDay = "Night";
+    styles.color = "green"
+  }
+  return <h1 style={styles}>Good{timeOfDay}!</h1>;
 }
 export default App;
